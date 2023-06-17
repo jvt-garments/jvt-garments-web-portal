811dae9cc4f892bc4a27c4c4a4d31c0b2208bb80
Umang <patelumang37@gmail.com>
2023-06-17 23:38:41 +0530

    Add functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,5 @@
+var allProducts = [];
+
 function formatCategory(category) {
   // replace dashes with spaces and capitalize first letter of each word
   return category.replace(/-/g, " ").replace(/\b\w/g, (l) => l.toUpperCase());
@@ -7,7 +9,9 @@ function createProductCard(product) {
   const { category, price, code, filename } = product;
   return `<div class="border px-2 py-2 rounded-xl shadow-xl">
             <div class="text-center">
+              <a href="/images/${category}/${filename}" data-lightbox="image-1" data-title="${code} - ($${price})">
                 <img src="/images/${category}/${filename}" alt="${code}" class="w-[200px] mx-auto" />
+              </a>
             </div>
             <div class="text-center py-2">
                 <div class="text-[16px] mb-2">${code}</div>
@@ -16,13 +20,21 @@ function createProductCard(product) {
         </div>`;
 }
 
+function fetchCategoryProducts(category) {
+  $("#productGrid").empty();
+  allProducts.forEach((item) => {
+    if (item.category === category) {
+      $("#productGrid").append(createProductCard(item));
+    }
+  });
+}
+
 $(document).ready(function () {
-  var allProducts = [];
   const categories = [];
   $.getJSON("data/data.json", function (data) {
     allProducts = data;
 
-    data.forEach((item) => {
+    data.forEach((item, index) => {
       const { category, price, code, filename } = item;
       const categoryName = formatCategory(category);
       if (!categories.includes(categoryName)) {
@@ -34,18 +46,16 @@ $(document).ready(function () {
         );
       }
 
-      $("#productGrid").append(createProductCard(item));
+      if (index === 0) {
+        fetchCategoryProducts(category);
+        $(".btn-category").first().addClass("active");
+      }
     });
   });
 
   $("body").on("click", ".btn-category", function () {
     $(this).addClass("active").siblings().removeClass("active");
     const category = $(this).data("id");
-    $("#productGrid").empty();
-    allProducts.forEach((item) => {
-      if (item.category === category) {
-        $("#productGrid").append(createProductCard(item));
-      }
-    });
+    fetchCategoryProducts(category);
   });
 });
