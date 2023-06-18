83d9fac89c7193db2aafab03e880aca49b9761cc
jaypopat <contact.jaypopat@gmail.com>
2023-06-19 00:33:41 +0100

    added categories in order of preference functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,5 @@
 var allProducts = [];
+var allProductsCategories = [];
 
 function formatCategory(category) {
   // replace dashes with spaces and capitalize first letter of each word
@@ -9,13 +10,13 @@ function createProductCard(product) {
   const { category, price, code, filename } = product;
   return `<div class="border px-2 py-2 rounded-xl shadow-xl">
             <div class="text-center">
-              <a href="images/${category}/${filename}" data-lightbox="image-1" data-title="${code} - ($${price})">
+              <a href="images/${category}/${filename}" data-lightbox="image-1" data-title="${code} - (€${price})">
                 <img src="images/${category}/${filename}" alt="${code}" class="w-[200px] mx-auto" />
               </a>
             </div>
             <div class="text-center py-2">
                 <div class="text-[16px] mb-2">${code}</div>
-                <div class="font-bold">$${price}</div>
+                <div class="font-bold">€${price}</div>
             </div>
         </div>`;
 }
@@ -30,27 +31,30 @@ function fetchCategoryProducts(category) {
 }
 
 $(document).ready(function () {
-  const categories = [];
-  $.getJSON("data/data.json", function (data) {
-    allProducts = data;
+  $.getJSON("data/categories.json", function (data) {
+    allProductsCategories = data.filter((obj) => obj.enabled === true);
+    allProductsCategories.sort((a, b) => a.order - b.order);
+    console.log(allProductsCategories);
 
-    data.forEach((item, index) => {
-      const { category, price, code, filename } = item;
-      const categoryName = formatCategory(category);
-      if (!categories.includes(categoryName)) {
-        categories.push(categoryName);
-        $("#categoryTabs").append(
-          `<div class="tab min-w-[150px] px-2 py-2 text-center btn-category" data-id="${category}" >
-                <span class="text-[14px]" >${categoryName}</span>
-            </div>`
-        );
-      }
-
-      if (index === 0) {
-        fetchCategoryProducts(category);
-        $(".btn-category").first().addClass("active");
-      }
+    allProductsCategories.forEach((element) => {
+      console.log(element.name);
+      $("#categoryTabs").append(
+        `<div class="tab min-w-[150px] px-2 py-2 text-center btn-category" data-id="${
+          element.name
+        }" >
+              <span class="text-[14px]" >${formatCategory(element.name)}</span>
+          </div>`
+      );
     });
+  });
+
+  $.getJSON("data/data.json", function (data) {
+
+    allProducts = data;
+    
+    fetchCategoryProducts(allProductsCategories[0].name);
+    $(".btn-category").first().addClass("active");
+
   });
 
   $("body").on("click", ".btn-category", function () {
